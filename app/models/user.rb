a3356092d335662cabbdc9e75d8311c745e6795a
joshnevius <joshnevius92@gmail.com>
2016-08-30 14:39:51 -0400

    Added omniauth and omniauth-facebook gems, updated devise_for :users route to include omniauth callbacks for controller. Updated User model to be devise: omniauthable. Also edited lists/index to have short greeting for the current user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
   has_many :lists
   has_many :tasks, through: :lists
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :lists do
     resources :tasks
   end
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
