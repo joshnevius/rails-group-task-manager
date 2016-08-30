7adfdb68bc9e2b4d8de242a37da09fcfeadbce3b
joshnevius <joshnevius92@gmail.com>
2016-08-30 17:53:33 -0400

    Added create method to Tasks controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+  def create
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.build(task_params)
+    if @task.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show"
+    end
+  end
+
 end
