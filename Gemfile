500ae8e32ec2c5fca27f6af641128026a2ff8b0c
joshnevius <joshnevius92@gmail.com>
2016-08-30 17:25:10 -0400

    Changed out social media site from Facebook to Twitter and changed and bundled gem, changed configuration for omniauth in devise.rb, added omniauthable: to User model and .from_omniauth, also messed with links in layout page to be more rails-like.

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'devise'
 
 gem 'omniauth'
 
-gem 'omniauth-facebook'
+gem 'omniauth-twitter'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  devise :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :lists
   has_many :tasks, through: :lists
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
     end
   end
 end
