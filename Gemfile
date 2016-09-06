de8c20282aec3187b59e5c5f9bbab99400397967
joshnevius <joshnevius92@gmail.com>
2016-09-06 12:52:21 -0400

    Added .from_omniauth method in User model, added users::omniauthcallback class with github method to allow github to sign in and redirect user. Also added omniauth gem.

--- Gemfile
+++ Gemfile
@@ -24,8 +24,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
-
-# gem 'omniauth'
+ 
+gem 'omniauth'
 
 gem 'omniauth-github'
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
   has_many :user_lists
   has_many :lists, through: :user_lists
 
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]
-  #   end      
-  # end
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end      
+  end
 end
